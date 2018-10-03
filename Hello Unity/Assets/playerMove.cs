2f01179aa14698532326ae81d68a49478be96e1c
Helder Lima de Jesus <helder.jesus@fatec.sp.gov.br>
2018-10-03 20:56:10 -0300

    Player pulando
    Pulando e andando

--- Hello Unity/Assets/playerMove.cs
+++ Hello Unity/Assets/playerMove.cs
@@ -6,14 +6,21 @@ public class playerMove : MonoBehaviour {
 	public CharacterController2D controller;
 	public float runSpeed = 40f;
 	float horizontalMove = 0f;
+    bool jump = false;
 
 	// Update is called once per frame
 	void Update () {
 		//Debug.Log(Input.GetAxisRaw("Horizontal"));
 		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
 	}
 
 	void FixedUpdate () {
-		controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
-	}
+		controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
 }

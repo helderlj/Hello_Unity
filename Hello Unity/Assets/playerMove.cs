0f0dcb7b21bf2adc45916c53970f1839c7952fb4
Helder Lima de Jesus <helder.jesus@fatec.sp.gov.br>
2018-10-03 23:32:18 -0300

    Animations in place

--- Hello Unity/Assets/playerMove.cs
+++ Hello Unity/Assets/playerMove.cs
@@ -4,23 +4,42 @@ using UnityEngine;
 
 public class playerMove : MonoBehaviour {
 	public CharacterController2D controller;
+    public Animator animator;
 	public float runSpeed = 40f;
 	float horizontalMove = 0f;
     bool jump = false;
-
+    bool crouch = false;
 	// Update is called once per frame
 	void Update () {
 		//Debug.Log(Input.GetAxisRaw("Horizontal"));
 		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
-
+        animator.SetFloat("speed", Mathf.Abs(horizontalMove));
         if (Input.GetButtonDown("Jump"))
         {
             jump = true;
+            animator.SetBool("isJumping", true);
+        }
+
+        if (Input.GetButtonDown("Crouch")){
+            crouch = true;
+        } else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
         }
 	}
 
+    public void OnLanding()
+    {
+        animator.SetBool("isJumping", false);
+    }
+
+    public void OnCrouching(bool isCrouching)
+    {
+        animator.SetBool("isCrouching", isCrouching);
+    }
+
 	void FixedUpdate () {
-		controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+		controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
         jump = false;
     }
 }
